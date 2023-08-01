8d673e6259b425f9f4857a325008bdc544ab0377
soomanbaek <qortnaks311@gmail.com>
2023-08-01 22:47:24 +0900

    initial commit

---
+++ src/main/java/blacktokkies/toquiz/ToquizApplication.java
@@ -0,0 +1,13 @@
+package blacktokkies.toquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToquizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToquizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/blacktokkies/toquiz/ToquizApplicationTests.java
@@ -0,0 +1,13 @@
+package blacktokkies.toquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToquizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
