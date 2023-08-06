43b6a359834e6d4faff1a93e11747e7390ad947f
soomanbaek <qortnaks311@gmail.com>
2023-08-06 17:30:54 +0900

    feat: 전역 예외 처리 핸들러 생성 (#7)

---
+++ src/main/java/blacktokkies/toquiz/common/error/handler/GlobalExceptionHandler.java
@@ -0,0 +1,100 @@
+package blacktokkies.toquiz.common.error.handler;
+
+import blacktokkies.toquiz.common.error.errorcode.CommonErrorCode;
+import blacktokkies.toquiz.common.error.errorcode.ErrorCode;
+import blacktokkies.toquiz.common.error.exception.RestApiException;
+import blacktokkies.toquiz.common.error.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import java.util.List;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(RestApiException.class)
+    public ResponseEntity<Object> handleCustomException(RestApiException e){
+        final ErrorCode errorCode = e.getErrorCode();
+        return handleExceptionInternal(errorCode);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException e){
+        log.warn("handleIllegalArgument", e);
+        final ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
+        return handleExceptionInternal(errorCode, e.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException e,
+        HttpHeaders httpHeaders,
+        HttpStatusCode status,
+        WebRequest webRequest) {
+
+        log.warn("handleIllegalArgument", e);
+        final ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
+
+        return handleExceptionInternal(e, errorCode);
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Object> handleAllException(Exception e) {
+        log.warn("handleAllException", e);
+        final ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
+        return handleExceptionInternal(errorCode);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+            .body(makeErrorResponse(errorCode));
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode, String message) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+            .body(makeErrorResponse(errorCode, message));
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(BindException e, ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+            .body(makeErrorResponse(e, errorCode));
+    }
+
+    private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
+        return ErrorResponse.builder()
+            .code(errorCode.name())
+            .statusCode(errorCode.getHttpStatus().value())
+            .message(errorCode.getMessage())
+            .build();
+    }
+
+    private ErrorResponse makeErrorResponse(ErrorCode errorCode, String message) {
+        return ErrorResponse.builder()
+            .code(errorCode.name())
+            .statusCode(errorCode.getHttpStatus().value())
+            .message(message)
+            .build();
+    }
+
+    private ErrorResponse makeErrorResponse(BindException e, ErrorCode errorCode) {
+        List<ErrorResponse.ValidationError> validationErrorList = e.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(ErrorResponse.ValidationError::of)
+            .toList();
+
+        return ErrorResponse.builder()
+            .code(errorCode.name())
+            .statusCode(errorCode.getHttpStatus().value())
+            .message(errorCode.getMessage())
+            .errors(validationErrorList)
+            .build();
+    }
+}
