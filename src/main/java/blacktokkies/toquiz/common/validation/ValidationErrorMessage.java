cac8c2ec244123c8d3290d55f6d6b941f866129b
soomanbaek <qortnaks311@gmail.com>
2023-08-07 14:27:05 +0900

    refactor: 항목이 비어있을 때 에러 메시지 변경
    - 변경 전: EMPTY_MESSAGE로 통일
    - 변경 후: 항목에 따른 EMPTY_MESSAGE

--- src/main/java/blacktokkies/toquiz/common/validation/ValidationErrorMessage.java
+++ src/main/java/blacktokkies/toquiz/common/validation/ValidationErrorMessage.java
@@ -6,9 +6,13 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class ValidationErrorMessage {
+    public static final String EMPTY_EMAIL_MESSAGE = "아이디 항목을 입력해주세요.";
+    public static final String EMPTY_PASSWORD_MESSAGE = "비밀번호 항목을 입력해주세요.";
+    public static final String EMPTY_NICKNAME_MESSAGE = "닉네임 항목을 입력해주세요.";
+
     public static final String MEMBER_PASSWORD_LENGTH_MESSAGE = "비밀번호는 8 ~ 20자 이어야 합니다.";
     public static final String MEMBER_PASSWORD_FORMAT_MESSAGE = "비밀번호는 영문자, 숫자, 특수기호를 반드시 포함해야 합니다.";
-    public static final String EMPTY_MESSAGE = "모든 항목을 입력해주세요.";
     public static final String MEMBER_EMAIL_MESSAGE = "아이디는 이메일 형식으로 입력해주세요.";
     public static final String MEMBER_NICKNAME_MESSAGE = "닉네임은 8 ~ 20자 이어야 합니다.";
+
 }
