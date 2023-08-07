7cb64857d83e61b1d641d340a6a1ac502afac7a9
soomanbaek <qortnaks311@gmail.com>
2023-08-07 14:30:02 +0900

    feat: `NotEmpty` -> `NotBlank` 변경 및 항목이 비어 있을 경우 에러 메세지 변경

--- src/main/java/blacktokkies/toquiz/member/dto/request/SignUpRequestDto.java
+++ src/main/java/blacktokkies/toquiz/member/dto/request/SignUpRequestDto.java
@@ -1,9 +1,9 @@
-package blacktokkies.toquiz.member.dto;
+package blacktokkies.toquiz.member.dto.request;
 
 import blacktokkies.toquiz.member.domain.Member;
 import blacktokkies.toquiz.member.domain.Provider;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,25 +16,25 @@ import static blacktokkies.toquiz.helper.PasswordEncryptor.encryptPassword;
 @Getter
 @NoArgsConstructor
 public class SignUpRequestDto {
-    @NotEmpty(message = EMPTY_MESSAGE)
+    @NotBlank(message = EMPTY_EMAIL_MESSAGE)
     @Email(message = MEMBER_EMAIL_MESSAGE)
     private String email;
 
-    @NotEmpty(message = EMPTY_MESSAGE)
+    @NotBlank(message = EMPTY_PASSWORD_MESSAGE)
     @Length(min = 8, max = 20, message = MEMBER_PASSWORD_LENGTH_MESSAGE)
     @Pattern(regexp = MEMBER_PASSWORD_FORMAT, message = MEMBER_PASSWORD_FORMAT_MESSAGE)
     private String password;
 
-    @NotEmpty(message = EMPTY_MESSAGE)
+    @NotBlank(message = EMPTY_NICKNAME_MESSAGE)
     @Length(min = 2, max = 20, message = MEMBER_NICKNAME_MESSAGE)
     private String nickname;
 
     public Member toMember(){
         return Member.builder()
-                .email(email)
-                .password(encryptPassword(password))
-                .nickname(nickname)
-                .provider(Provider.LOCAL)
-                .build();
+            .email(email)
+            .password(encryptPassword(password))
+            .nickname(nickname)
+            .provider(Provider.LOCAL)
+            .build();
     }
 }
