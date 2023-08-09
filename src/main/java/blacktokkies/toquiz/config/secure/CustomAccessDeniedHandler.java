746e1e73d95aa50137e7318742c1c1da1e908930
soomanbaek <qortnaks311@gmail.com>
2023-08-10 02:23:18 +0900

    feat: 인증되지 않은 사용자가 인증이 필요한 API 요청 시, `api/auth/refresh`로 redirect (#12)

---
+++ src/main/java/blacktokkies/toquiz/config/secure/CustomAccessDeniedHandler.java
@@ -0,0 +1,21 @@
+package blacktokkies.toquiz.config.secure;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.warn("Invalid User Request");
+        response.sendRedirect("/api/auth/refresh");
+    }
+}
