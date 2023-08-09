d04acafc9f22eae9199def2ab362b0f4c3d317a4
soomanbaek <qortnaks311@gmail.com>
2023-08-09 14:32:47 +0900

    feat: 성공 응답 형식

---
+++ src/main/java/blacktokkies/toquiz/common/success/SuccessMessage.java
@@ -0,0 +1,16 @@
+package blacktokkies.toquiz.common.success;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum SuccessMessage {
+    SIGN_UP(HttpStatus.OK.value(), "회원가입에 성공하였습니다.");
+
+    private final int statusCode;
+    private final String message;
+}
