249ea15c21b557cbcec6dd73b938683e6ff8a21c
soomanbaek <qortnaks311@gmail.com>
2023-08-10 02:28:42 +0900

    feat: API 요청 시, 로그를 남긴다 (#14)

---
+++ src/main/java/blacktokkies/toquiz/interceptor/LoggingInterceptor.java
@@ -0,0 +1,17 @@
+package blacktokkies.toquiz.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        log.info(request.getMethod() + " "+ request.getRequestURL());
+        return true;
+    }
+}
