edbd0e71fe74f90cefa111225b1c671745044406
soomanbaek <qortnaks311@gmail.com>
2023-08-09 14:38:26 +0900

    feat: login 관련 AuthErrorCode 추가

--- src/main/java/blacktokkies/toquiz/common/error/errorcode/AuthErrorCode.java
+++ src/main/java/blacktokkies/toquiz/common/error/errorcode/AuthErrorCode.java
@@ -1,15 +1,16 @@
-package blacktokkies.toquiz.member.errorcode;
+package blacktokkies.toquiz.common.error.errorcode;
 
-import blacktokkies.toquiz.common.error.errorcode.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum MemberErrorCode implements ErrorCode {
+public enum AuthErrorCode implements ErrorCode {
     DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 email 입니다."),
     DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 nickname 입니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    NOT_EXIST_MEMBER(HttpStatus.NOT_FOUND, "아이디가 존재하지 않습니다."),
     ;
 
     private final HttpStatus httpStatus;
