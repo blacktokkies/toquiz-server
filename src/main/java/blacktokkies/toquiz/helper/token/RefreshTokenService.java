3b066250bb1b55c44a30d4eedcd3a0bdc91df6c0
soomanbaek <qortnaks311@gmail.com>
2023-08-11 00:10:16 +0900

    feat: 유효한 RefreshToken인지 검증 (#13)

--- src/main/java/blacktokkies/toquiz/helper/token/RefreshTokenService.java
+++ src/main/java/blacktokkies/toquiz/helper/token/RefreshTokenService.java
@@ -1,9 +1,15 @@
 package blacktokkies.toquiz.helper.token;
 
+import blacktokkies.toquiz.common.error.exception.RestApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.Objects;
+
+import static blacktokkies.toquiz.common.error.errorcode.AuthErrorCode.INVALID_REFRESH_TOKEN;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +18,15 @@ public class RefreshTokenService {
     private Integer REFRESH_TOKEN_EXPIRATION;
 
     private final RefreshTokenRepository refreshTokenRepository;
+
+    public void validate(String refreshToken, String email){
+        RefreshToken savedRefreshToken = refreshTokenRepository.findByEmail(email)
+            .orElseThrow(() -> new RestApiException(INVALID_REFRESH_TOKEN));
+
+        if(!Objects.equals(refreshToken, savedRefreshToken.getRefreshToken())){
+            throw new RestApiException(INVALID_REFRESH_TOKEN);
+        }
+    }
 
     @Transactional
     public void save(String email, String refreshToken) {
