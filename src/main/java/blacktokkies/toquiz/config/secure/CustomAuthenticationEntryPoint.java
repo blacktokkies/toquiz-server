889b2f97171c47324a80710a144651d4d4f9a986
soomanbaek <qortnaks311@gmail.com>
2023-08-10 17:12:35 +0900

    feat: 유효하지 않은 액세스 토큰 시 에러 메시지 응답

--- src/main/java/blacktokkies/toquiz/config/secure/CustomAuthenticationEntryPoint.java
+++ src/main/java/blacktokkies/toquiz/config/secure/CustomAuthenticationEntryPoint.java
@@ -1,8 +1,12 @@
 package blacktokkies.toquiz.config.secure;
 
-import jakarta.servlet.ServletException;
+import blacktokkies.toquiz.common.error.errorcode.ErrorCode;
+import blacktokkies.toquiz.common.error.response.ErrorResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -10,13 +14,32 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+import static blacktokkies.toquiz.common.error.errorcode.AuthErrorCode.INVALID_ACCESS_TOKEN;
+
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException {
+                         AuthenticationException authException) throws IOException  {
         log.warn("Invalid User Request" + request.getRequestURI());
-        response.sendRedirect("localhost://8080/api/auth/refresh");
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(makeErrorResponse(INVALID_ACCESS_TOKEN));
+    }
+
+    private String makeErrorResponse(ErrorCode errorCode) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+            .code(errorCode.name())
+            .statusCode(errorCode.getHttpStatus().value())
+            .message(errorCode.getMessage())
+            .build();
+
+        return mapper.writeValueAsString(errorResponse);
     }
 }
