6fa1ee33647f987832667617a62bb3192293bf3c
soomanbaek <qortnaks311@gmail.com>
2023-08-13 02:32:26 +0900

    feat: 회원 탈퇴 기능 (#17)

--- src/main/java/blacktokkies/toquiz/domain/member/api/MemberApi.java
+++ src/main/java/blacktokkies/toquiz/domain/member/api/MemberApi.java
@@ -1,10 +1,12 @@
 package blacktokkies.toquiz.domain.member.api;
 
 import blacktokkies.toquiz.domain.member.application.MemberService;
-import blacktokkies.toquiz.domain.member.dto.response.GetMyInfoResponse;
+import blacktokkies.toquiz.domain.member.dto.response.MemberInfoResponse;
+import blacktokkies.toquiz.global.common.response.SuccessMessage;
 import blacktokkies.toquiz.global.common.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +16,14 @@ public class MemberApi {
     private final MemberService memberService;
 
     @GetMapping("api/members/me")
-    public ResponseEntity<SuccessResponse<GetMyInfoResponse>> getMyInfo(){
-        GetMyInfoResponse response = memberService.getMyInfo();
+    public ResponseEntity<SuccessResponse<MemberInfoResponse>> getMyInfo(){
+        MemberInfoResponse response = memberService.getMyInfo();
         return ResponseEntity.ok(new SuccessResponse<>(response));
+    }
+
+    @DeleteMapping("api/members/me")
+    public ResponseEntity<SuccessMessage> deleteMyInfo(){
+        memberService.deleteMyInfo();
+        return ResponseEntity.ok(SuccessMessage.RESIGN);
     }
 }
