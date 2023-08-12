e6c7dee7b8e3a11e0a6443fac7e54f13213c2a83
soomanbaek <qortnaks311@gmail.com>
2023-08-13 01:36:12 +0900

    feat: 핸들러가 존재하지 않을 시 `404 NOT_FOUND` 에러 처리 (#23)

--- src/main/java/blacktokkies/toquiz/global/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/blacktokkies/toquiz/global/config/security/CustomAuthenticationEntryPoint.java
@@ -1,9 +1,6 @@
 package blacktokkies.toquiz.global.config.security;
 
-import blacktokkies.toquiz.global.common.error.errorcode.ErrorCode;
-import blacktokkies.toquiz.global.common.response.ErrorResponse;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -15,31 +12,20 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 import static blacktokkies.toquiz.domain.member.exception.MemberErrorCode.INVALID_ACCESS_TOKEN;
+import static blacktokkies.toquiz.global.common.error.errorcode.CommonErrorCode.RESOURCE_NOT_FOUND;
+import static blacktokkies.toquiz.global.config.security.SendErrorResponse.sendErrorResponse;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final HttpRequestEndpointChecker endpointChecker;
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException  {
-        log.warn("Invalid User Request" + request.getRequestURI());
-
-        response.setContentType("application/json");
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setCharacterEncoding("utf-8");
-        response.getWriter().write(makeErrorResponse(INVALID_ACCESS_TOKEN));
-    }
-
-    private String makeErrorResponse(ErrorCode errorCode) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-
-        ErrorResponse errorResponse = ErrorResponse.builder()
-            .code(errorCode.name())
-            .statusCode(errorCode.getHttpStatus().value())
-            .message(errorCode.getMessage())
-            .build();
-
-        return mapper.writeValueAsString(errorResponse);
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        if(!endpointChecker.isEndpointExist(request)){
+            sendErrorResponse(response, RESOURCE_NOT_FOUND);
+            return;
+        }
+        sendErrorResponse(response, INVALID_ACCESS_TOKEN);
     }
 }
