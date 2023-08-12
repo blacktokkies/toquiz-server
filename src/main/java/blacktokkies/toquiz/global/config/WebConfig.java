c6d37a54cbb062b8a6ce298cb64991d2fc2f48ad
soomanbaek <qortnaks311@gmail.com>
2023-08-13 01:32:29 +0900

    rename: `LoggingInterceptor` 폴더 이동 (#18)
    - `global/common/interceptor` -> `global/common/log`

--- src/main/java/blacktokkies/toquiz/global/config/WebConfig.java
+++ src/main/java/blacktokkies/toquiz/global/config/WebConfig.java
@@ -1,6 +1,6 @@
 package blacktokkies.toquiz.global.config;
 
-import blacktokkies.toquiz.global.common.interceptor.LoggingInterceptor;
+import blacktokkies.toquiz.global.common.log.LoggingInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
