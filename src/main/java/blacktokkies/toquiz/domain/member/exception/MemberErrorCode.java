ba853f532ca7aed19b8794a937b2146dbbb7c188
soomanbaek <qortnaks311@gmail.com>
2023-08-12 21:24:35 +0900

    refactor: 비밀번호 불일치 에러 코드 변경
    - `INVALID_PASSWORD` -> `NOT_MATCH_PASSWORD`

--- src/main/java/blacktokkies/toquiz/domain/member/exception/MemberErrorCode.java
+++ src/main/java/blacktokkies/toquiz/domain/member/exception/MemberErrorCode.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum MemberErrorCode implements ErrorCode {
     DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 email 입니다."),
     DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 nickname 입니다."),
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     NOT_EXIST_MEMBER(HttpStatus.NOT_FOUND, "아이디가 존재하지 않습니다."),
     NOT_LOGIN_MEMBER(HttpStatus.FORBIDDEN, "로그인된 사용자가 아닙니다."),
     INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "액세스 토큰이 유효하지 않습니다."),
