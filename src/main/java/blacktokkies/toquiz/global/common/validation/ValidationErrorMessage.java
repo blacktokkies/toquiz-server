f7b4f7947b7c81fe025e42e3f822efe381a43edd
soomanbaek <qortnaks311@gmail.com>
2023-08-13 05:07:54 +0900

    feat: 패널 생성 요청 title과 description의 validation error message (#28)

--- src/main/java/blacktokkies/toquiz/global/common/validation/ValidationErrorMessage.java
+++ src/main/java/blacktokkies/toquiz/global/common/validation/ValidationErrorMessage.java
@@ -10,10 +10,15 @@ public class ValidationErrorMessage {
     public static final String EMAIL_FORMAT_MESSAGE = "아이디는 이메일 형식으로 입력해주세요.";
 
     public static final String PASSWORD_EMPTY_MESSAGE = "비밀번호 항목을 입력해주세요.";
-    public static final String PASSWORD_LENGTH_MESSAGE = "비밀번호는 8 ~ 20자 이어야 합니다.";
+    public static final String PASSWORD_LENGTH_MESSAGE = "비밀번호는 8~20자 이어야 합니다.";
     public static final String PASSWORD_FORMAT_MESSAGE = "비밀번호는 영문자, 숫자, 특수기호를 반드시 포함해야 합니다.";
 
     public static final String NICKNAME_EMPTY_MESSAGE = "닉네임 항목을 입력해주세요.";
-    public static final String NICKNAME_LENGTH_MESSAGE = "닉네임은 2 ~ 20자 이어야 합니다.";
+    public static final String NICKNAME_LENGTH_MESSAGE = "닉네임은 2~20자 이어야 합니다.";
+
+    public static final String TITLE_EMPTY_MESSAGE = "제목을 입력해주세요.";
+    public static final String TITLE_LENGTH_MESSAGE = "제목은 3~40자 이어야 합니다.";
+
+    public static final String DESCRIPTION_LENGTH_MESSAGE = "설명은 50자 이하 이어야 합니다.";
 
 }
