bef0eadb5be1f16827e4dfebfd17c5e0a2f6e9ee
soomanbaek <qortnaks311@gmail.com>
2023-08-13 01:28:03 +0900

    feat: ErrorCode 추가

--- src/main/java/blacktokkies/toquiz/global/common/error/errorcode/CommonErrorCode.java
+++ src/main/java/blacktokkies/toquiz/global/common/error/errorcode/CommonErrorCode.java
@@ -10,6 +10,9 @@ public enum CommonErrorCode implements ErrorCode{
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid parameter included"),
     REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "Resource not exists"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
+    USER_NOT_FOUND(HttpStatus.FORBIDDEN, "User is not exist"),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "Access denied"),
+    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "Resource not found"),
     ;
 
     private final HttpStatus httpStatus;
