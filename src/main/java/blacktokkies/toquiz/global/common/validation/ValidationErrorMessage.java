76b352ab5ed044d0fd579c7909044e315b44410c
soomanbaek <qortnaks311@gmail.com>
2023-08-13 04:30:41 +0900

    fix: nickname length validation error 메시지

--- src/main/java/blacktokkies/toquiz/global/common/validation/ValidationErrorMessage.java
+++ src/main/java/blacktokkies/toquiz/global/common/validation/ValidationErrorMessage.java
@@ -14,6 +14,6 @@ public class ValidationErrorMessage {
     public static final String PASSWORD_FORMAT_MESSAGE = "비밀번호는 영문자, 숫자, 특수기호를 반드시 포함해야 합니다.";
 
     public static final String NICKNAME_EMPTY_MESSAGE = "닉네임 항목을 입력해주세요.";
-    public static final String NICKNAME_LENGTH_MESSAGE = "닉네임은 8 ~ 20자 이어야 합니다.";
+    public static final String NICKNAME_LENGTH_MESSAGE = "닉네임은 2 ~ 20자 이어야 합니다.";
 
 }
