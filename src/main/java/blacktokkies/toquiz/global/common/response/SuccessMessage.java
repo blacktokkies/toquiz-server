c0e9cffc32120d7d1e9d740f65acb5c517c925c5
soomanbaek <qortnaks311@gmail.com>
2023-08-13 02:25:24 +0900

    refactor: 로그아웃 성공 메시지 수정 (#12)

--- src/main/java/blacktokkies/toquiz/global/common/response/SuccessMessage.java
+++ src/main/java/blacktokkies/toquiz/global/common/response/SuccessMessage.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum SuccessMessage {
     SIGN_UP(HttpStatus.OK.value(), "회원가입에 성공하였습니다."),
-    LOG_OUT(HttpStatus.OK.value(), "로그아웃에 성공하였습니다."),
+    LOGOUT(HttpStatus.OK.value(), "로그아웃에 성공하였습니다."),
     ;
 
     private final int statusCode;
