543dd9eb855d97bc79102a614533a1ad9a866ced
soomanbaek <qortnaks311@gmail.com>
2023-08-12 21:25:24 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/blacktokkies/toquiz/global/util/auth/CookieService.java
+++ src/main/java/blacktokkies/toquiz/global/util/auth/CookieService.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
