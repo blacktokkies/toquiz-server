a192560515512c2f5e12941afc110559715a7ea6
soomanbaek <qortnaks311@gmail.com>
2023-08-13 20:34:30 +0900

    feat: 패널 삭제 성공 메시지 (#31)

--- src/main/java/blacktokkies/toquiz/global/common/response/SuccessMessage.java
+++ src/main/java/blacktokkies/toquiz/global/common/response/SuccessMessage.java
@@ -12,6 +12,7 @@ public enum SuccessMessage {
     SIGN_UP(HttpStatus.OK.value(), "회원가입에 성공하였습니다."),
     LOGOUT(HttpStatus.OK.value(), "로그아웃에 성공하였습니다."),
     RESIGN(HttpStatus.OK.value(), "회원탈퇴에 성공하였습니다."),
+    PANEL_DELETE(HttpStatus.OK.value(), "패널 삭제에 성공하였습니다."),
     ;
 
     private final int statusCode;
