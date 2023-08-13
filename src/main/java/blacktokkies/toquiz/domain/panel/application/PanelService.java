f2e19b3c318c6f0a9597830806bad5cf57a949eb
soomanbaek <qortnaks311@gmail.com>
2023-08-13 17:12:02 +0900

    feat: 자기 자신의 패널 가져오기 (#30)

--- src/main/java/blacktokkies/toquiz/domain/panel/application/PanelService.java
+++ src/main/java/blacktokkies/toquiz/domain/panel/application/PanelService.java
@@ -4,11 +4,14 @@ import blacktokkies.toquiz.domain.member.domain.Member;
 import blacktokkies.toquiz.domain.panel.dao.PanelRepository;
 import blacktokkies.toquiz.domain.panel.domain.Panel;
 import blacktokkies.toquiz.domain.panel.dto.request.CreatePanelRequest;
-import blacktokkies.toquiz.domain.panel.dto.response.CreatePanelResponse;
+import blacktokkies.toquiz.domain.panel.dto.response.PanelResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,9 +20,17 @@ public class PanelService {
     private final PanelRepository panelRepository;
 
     @Transactional
-    public CreatePanelResponse create(CreatePanelRequest createPanelRequest) {
+    public PanelResponse create(CreatePanelRequest createPanelRequest) {
         Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Panel panel = panelRepository.save(createPanelRequest.toPanelWith(member));
-        return CreatePanelResponse.toDto(panel);
+
+        return PanelResponse.toDto(panel);
+    }
+
+    public List<PanelResponse> getMyPanels(Pageable pageable){
+        Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<Panel> panels = panelRepository.findAllByMember(member, pageable);
+
+        return panels.stream().map(PanelResponse::toDto).toList();
     }
 }
