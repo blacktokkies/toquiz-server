f73988664a12e3185288ce19359d501a1c478a64
soomanbaek <qortnaks311@gmail.com>
2023-08-14 03:06:13 +0900

    feat: 질문 가져오기 (#36)

--- src/main/java/blacktokkies/toquiz/domain/question/api/QuestionApi.java
+++ src/main/java/blacktokkies/toquiz/domain/question/api/QuestionApi.java
@@ -2,24 +2,41 @@ package blacktokkies.toquiz.domain.question.api;
 
 import blacktokkies.toquiz.domain.question.application.QuestionService;
 import blacktokkies.toquiz.domain.question.dto.request.CreateQuestionRequest;
-import blacktokkies.toquiz.domain.question.dto.response.CreateQuestionResponse;
+import blacktokkies.toquiz.domain.question.dto.response.GetQuestionsResponse;
+import blacktokkies.toquiz.domain.question.dto.response.QuestionResponse;
 import blacktokkies.toquiz.global.common.response.SuccessResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class QuestionApi {
     private final QuestionService questionService;
     @PostMapping("api/panels/{panelId}/question")
-    public ResponseEntity<SuccessResponse<CreateQuestionResponse>> createQuestion(
+    public ResponseEntity<SuccessResponse<QuestionResponse>> createQuestion(
         @RequestBody @Valid CreateQuestionRequest request,
         @CookieValue("active_info_id") String activeInfoId,
         @PathVariable Long panelId
     ){
-        CreateQuestionResponse response = questionService.createQuestion(request, activeInfoId, panelId);
+        QuestionResponse response = questionService.createQuestion(request, activeInfoId, panelId);
+
+        return ResponseEntity.ok(new SuccessResponse<>(response));
+    }
+
+    @GetMapping("api/panels/{panelId}/questions")
+    public ResponseEntity<SuccessResponse<GetQuestionsResponse>> getQuestions(
+        @PageableDefault(size = 30, sort = {"likeNum", "createdDate"}, direction = Sort.Direction.DESC) Pageable pageable,
+        @PathVariable Long panelId
+    ){
+        List<QuestionResponse> questionResponses = questionService.getQuestions(panelId, pageable);
+        GetQuestionsResponse response = GetQuestionsResponse.toDto(questionResponses, pageable);
 
         return ResponseEntity.ok(new SuccessResponse<>(response));
     }
--- src/main/java/blacktokkies/toquiz/domain/question/application/QuestionService.java
+++ src/main/java/blacktokkies/toquiz/domain/question/application/QuestionService.java
@@ -5,11 +5,14 @@ import blacktokkies.toquiz.domain.panel.domain.Panel;
 import blacktokkies.toquiz.domain.question.dao.QuestionRepository;
 import blacktokkies.toquiz.domain.question.domain.Question;
 import blacktokkies.toquiz.domain.question.dto.request.CreateQuestionRequest;
-import blacktokkies.toquiz.domain.question.dto.response.CreateQuestionResponse;
+import blacktokkies.toquiz.domain.question.dto.response.QuestionResponse;
 import blacktokkies.toquiz.global.common.error.RestApiException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static blacktokkies.toquiz.domain.panel.exception.PanelErrorCode.NOT_EXIST_PANEL;
 
@@ -21,18 +24,24 @@ public class QuestionService {
     private final PanelRepository panelRepository;
 
     @Transactional
-    public CreateQuestionResponse createQuestion(
+    public QuestionResponse createQuestion(
         CreateQuestionRequest createQuestionRequest,
         String activeInfoId,
         Long panelId)
     {
         Question question = questionRepository.save(new Question(createQuestionRequest.getContent(), getPanel(panelId), activeInfoId));
 
-        return CreateQuestionResponse.toDto(question);
+        return QuestionResponse.toDto(question);
     }
 
     private Panel getPanel(Long panelId){
         return panelRepository.findById(panelId).orElseThrow(
             () -> new RestApiException(NOT_EXIST_PANEL));
+    }
+
+    public List<QuestionResponse> getQuestions(Long panelId, Pageable pageable) {
+        List<Question> questions = questionRepository.findAllByPanel(getPanel(panelId), pageable);
+
+        return questions.stream().map(QuestionResponse::toDto).toList();
     }
 }
