e5196fd07a282652dbb327c57e4c26634e28eeb1
soomanbaek <qortnaks311@gmail.com>
2023-08-14 00:54:27 +0900

    feat: `Create Question` validation error message (#32)

--- src/main/java/blacktokkies/toquiz/global/common/validation/ValidationErrorMessage.java
+++ src/main/java/blacktokkies/toquiz/global/common/validation/ValidationErrorMessage.java
@@ -21,4 +21,6 @@ public class ValidationErrorMessage {
 
     public static final String DESCRIPTION_LENGTH_MESSAGE = "설명은 50자 이하 이어야 합니다.";
 
+    public static final String QUESTION_CONTENT_EMPTY_MESSAGE = "질문 내용을 입력해주세요.";
+    public static final String QUESTION_CONTENT_LENGTH_MESSAGE = "질문은 200자 이하 이어야 합니다.";
 }
