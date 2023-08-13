9485b2645783ce5378d10a1e783abe6d42da39e8
soomanbaek <qortnaks311@gmail.com>
2023-08-13 20:34:48 +0900

    feat: 패널 삭제 (#31)

--- src/main/java/blacktokkies/toquiz/domain/panel/api/PanelApi.java
+++ src/main/java/blacktokkies/toquiz/domain/panel/api/PanelApi.java
@@ -4,16 +4,14 @@ import blacktokkies.toquiz.domain.panel.application.PanelService;
 import blacktokkies.toquiz.domain.panel.dto.request.CreatePanelRequest;
 import blacktokkies.toquiz.domain.panel.dto.response.GetMyPanelsResponse;
 import blacktokkies.toquiz.domain.panel.dto.response.PanelResponse;
+import blacktokkies.toquiz.global.common.response.SuccessMessage;
 import blacktokkies.toquiz.global.common.response.SuccessResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,5 +36,12 @@ public class PanelApi {
         GetMyPanelsResponse response = GetMyPanelsResponse.dto(panels, pageable);
 
         return ResponseEntity.ok(new SuccessResponse<>(response));
+    }
+
+    @DeleteMapping("api/panels/{panelId}")
+    public ResponseEntity<SuccessMessage> deletePanel(@PathVariable("panelId") Long panelId){
+        panelService.deletePanel(panelId);
+
+        return ResponseEntity.ok(SuccessMessage.PANEL_DELETE);
     }
 }
--- src/main/java/blacktokkies/toquiz/domain/panel/application/PanelService.java
+++ src/main/java/blacktokkies/toquiz/domain/panel/application/PanelService.java
@@ -5,6 +5,7 @@ import blacktokkies.toquiz.domain.panel.dao.PanelRepository;
 import blacktokkies.toquiz.domain.panel.domain.Panel;
 import blacktokkies.toquiz.domain.panel.dto.request.CreatePanelRequest;
 import blacktokkies.toquiz.domain.panel.dto.response.PanelResponse;
+import blacktokkies.toquiz.global.common.error.RestApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,6 +13,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
+
+import static blacktokkies.toquiz.domain.panel.exception.PanelErrorCode.NOT_AUTHORIZED_DELETE;
+import static blacktokkies.toquiz.domain.panel.exception.PanelErrorCode.NOT_EXIST_PANEL;
 
 @Service
 @RequiredArgsConstructor
@@ -21,16 +26,41 @@ public class PanelService {
 
     @Transactional
     public PanelResponse create(CreatePanelRequest createPanelRequest) {
-        Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Member member = getMember();
         Panel panel = panelRepository.save(createPanelRequest.toPanelWith(member));
 
         return PanelResponse.toDto(panel);
     }
 
     public List<PanelResponse> getMyPanels(Pageable pageable){
-        Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Member member = getMember();
         List<Panel> panels = panelRepository.findAllByMember(member, pageable);
 
         return panels.stream().map(PanelResponse::toDto).toList();
+    }
+
+    @Transactional
+    public void deletePanel(Long panelId) {
+        checkIsAuthorizedToDelete(panelId);
+
+        panelRepository.deleteById(panelId);
+    }
+
+    private void checkIsAuthorizedToDelete(Long panelId) {
+        Panel panel = getPanel(panelId);
+        Member member = getMember();
+
+        if(!Objects.equals(member.getId(), panel.getMember().getId())){
+            throw new RestApiException(NOT_AUTHORIZED_DELETE);
+        }
+    }
+
+    private Panel getPanel(Long panelId){
+        return panelRepository.findById(panelId)
+            .orElseThrow(() -> new RestApiException(NOT_EXIST_PANEL));
+    }
+
+    private Member getMember() {
+        return (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
