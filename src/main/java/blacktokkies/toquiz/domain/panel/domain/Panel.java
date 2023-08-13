55a0e30af8bf1c043df8f382c9837c9806df542e
soomanbaek <qortnaks311@gmail.com>
2023-08-13 17:01:42 +0900

    feat: `Panel`, `Member` 양방향 연관관계
    - 연관관계 주인: `Panel`

--- src/main/java/blacktokkies/toquiz/domain/panel/domain/Panel.java
+++ src/main/java/blacktokkies/toquiz/domain/panel/domain/Panel.java
@@ -15,7 +15,7 @@ public class Panel extends BaseTime {
     @Column(name = "panel_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
