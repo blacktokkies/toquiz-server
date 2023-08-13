5f65ad8cafcbbc90faba2d783ae683f545df1ea1
soomanbaek <qortnaks311@gmail.com>
2023-08-14 00:55:19 +0900

    feat: `Create Question` Request DTO (#32)

---
+++ src/main/java/blacktokkies/toquiz/domain/question/dto/request/CreateQuestionRequest.java
@@ -0,0 +1,17 @@
+package blacktokkies.toquiz.domain.question.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import static blacktokkies.toquiz.global.common.validation.ValidationErrorMessage.QUESTION_CONTENT_EMPTY_MESSAGE;
+import static blacktokkies.toquiz.global.common.validation.ValidationErrorMessage.QUESTION_CONTENT_LENGTH_MESSAGE;
+
+@Getter
+@NoArgsConstructor
+public class CreateQuestionRequest {
+    @NotBlank(message = QUESTION_CONTENT_EMPTY_MESSAGE)
+    @Length(max=200, message = QUESTION_CONTENT_LENGTH_MESSAGE)
+    private String content;
+}
