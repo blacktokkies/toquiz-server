b61dfacb8cc9dae9602e14d71882b996c4ae7b5b
soomanbaek <qortnaks311@gmail.com>
2023-08-13 16:02:01 +0900

    feat: `MemberService` 트랜잭션 추가

--- src/main/java/blacktokkies/toquiz/domain/member/application/MemberService.java
+++ src/main/java/blacktokkies/toquiz/domain/member/application/MemberService.java
@@ -6,9 +6,11 @@ import blacktokkies.toquiz.domain.member.dto.response.MemberInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
     public MemberInfoResponse getMyInfo() {
@@ -16,6 +18,7 @@ public class MemberService {
         return MemberInfoResponse.toDto(member);
     }
 
+    @Transactional
     public void deleteMyInfo(){
         Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         memberRepository.delete(member);
