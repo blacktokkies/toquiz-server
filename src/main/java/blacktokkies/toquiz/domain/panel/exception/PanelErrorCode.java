55764884ee02493488f76dc0f50888a71b22415d
soomanbaek <qortnaks311@gmail.com>
2023-08-13 20:33:33 +0900

    feat: 패널 삭제 에러 코드 (#31)
    - `NOT_EXIST_PANEL` -> 403
    - `NOT_AUTHORIZED_DELETE` -> 404

---
+++ src/main/java/blacktokkies/toquiz/domain/panel/exception/PanelErrorCode.java
@@ -0,0 +1,17 @@
+package blacktokkies.toquiz.domain.panel.exception;
+
+import blacktokkies.toquiz.global.common.error.errorcode.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum PanelErrorCode implements ErrorCode {
+    NOT_EXIST_PANEL(HttpStatus.NOT_FOUND, "해당 패널이 존재하지 않습니다."),
+    NOT_AUTHORIZED_DELETE(HttpStatus.FORBIDDEN, "삭제 권한이 없습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
