c7830f9387b3199a9efa2e9f440d6a3dea5c586d
soomanbaek <qortnaks311@gmail.com>
2023-08-14 03:06:13 +0900

    feat: 질문 가져오기 (#36)

--- src/main/java/blacktokkies/toquiz/domain/question/dao/QuestionRepository.java
+++ src/main/java/blacktokkies/toquiz/domain/question/dao/QuestionRepository.java
@@ -1,9 +1,14 @@
 package blacktokkies.toquiz.domain.question.dao;
 
+import blacktokkies.toquiz.domain.panel.domain.Panel;
 import blacktokkies.toquiz.domain.question.domain.Question;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findAllByPanel(Panel panel, Pageable pageable);
 }
