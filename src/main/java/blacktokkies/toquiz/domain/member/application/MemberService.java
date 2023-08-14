3340117e7eb181b8e8af3d44dfe1855b2caa00ed
soomanbaek <qortnaks311@gmail.com>
2023-08-14 15:02:17 +0900

    feat: 사용자 정보 수정 (#16)

--- src/main/java/blacktokkies/toquiz/domain/member/application/MemberService.java
+++ src/main/java/blacktokkies/toquiz/domain/member/application/MemberService.java
@@ -2,6 +2,7 @@ package blacktokkies.toquiz.domain.member.application;
 
 import blacktokkies.toquiz.domain.member.dao.MemberRepository;
 import blacktokkies.toquiz.domain.member.domain.Member;
+import blacktokkies.toquiz.domain.member.dto.request.UpdateMyInfoRequest;
 import blacktokkies.toquiz.domain.member.dto.response.MemberInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,13 +15,27 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
     private final MemberRepository memberRepository;
     public MemberInfoResponse getMyInfo() {
-        Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return MemberInfoResponse.toDto(member);
+        return MemberInfoResponse.toDto(getMember());
     }
 
     @Transactional
     public void deleteMyInfo(){
-        Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        memberRepository.delete(member);
+        memberRepository.delete(getMember());
+    }
+
+    @Transactional
+    public MemberInfoResponse updateMyInfo(UpdateMyInfoRequest updateMyInfoRequest) {
+        Member member = getMember();
+        member.updateMemberInfo(
+            updateMyInfoRequest.getPassword(),
+            updateMyInfoRequest.getNickname()
+        );
+        Member updatedMember = memberRepository.save(member);
+
+        return MemberInfoResponse.toDto(updatedMember);
+    }
+
+    private Member getMember(){
+        return (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
