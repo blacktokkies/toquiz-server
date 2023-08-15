39b4d3658822b6c75528d2ca36e0820c644e4717
soomanbaek <qortnaks311@gmail.com>
2023-08-15 16:37:42 +0900

    feat: 패널 생성자가 아닐 경우 예외 코드 추가 (#46)

--- src/main/java/blacktokkies/toquiz/domain/panel/exception/PanelErrorCode.java
+++ src/main/java/blacktokkies/toquiz/domain/panel/exception/PanelErrorCode.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum PanelErrorCode implements ErrorCode {
     NOT_EXIST_PANEL(HttpStatus.NOT_FOUND, "해당 패널이 존재하지 않습니다."),
     NOT_AUTHORIZED_DELETE(HttpStatus.FORBIDDEN, "삭제 권한이 없습니다."),
+    NOT_CREATOR_PANEL(HttpStatus.FORBIDDEN, "패널 생성자가 아닙니다.")
     ;
 
     private final HttpStatus httpStatus;
