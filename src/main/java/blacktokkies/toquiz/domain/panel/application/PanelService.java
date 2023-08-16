13ae02648d36eb00ecba33aee4da208df58ad60d
soomanbaek <qortnaks311@gmail.com>
2023-08-16 22:06:51 +0900

    feat: 사용자 활동 정보 가져오기 (#54)

--- src/main/java/blacktokkies/toquiz/domain/panel/application/PanelService.java
+++ src/main/java/blacktokkies/toquiz/domain/panel/application/PanelService.java
@@ -1,13 +1,18 @@
 package blacktokkies.toquiz.domain.panel.application;
 
+import blacktokkies.toquiz.domain.activeinfo.ActiveInfoRepository;
+import blacktokkies.toquiz.domain.activeinfo.domain.ActiveInfo;
+import blacktokkies.toquiz.domain.activeinfo.domain.ActivePanel;
 import blacktokkies.toquiz.domain.member.domain.Member;
 import blacktokkies.toquiz.domain.panel.dao.PanelRepository;
 import blacktokkies.toquiz.domain.panel.domain.Panel;
 import blacktokkies.toquiz.domain.panel.dto.request.CreatePanelRequest;
 import blacktokkies.toquiz.domain.panel.dto.request.UpdatePanelRequest;
+import blacktokkies.toquiz.domain.panel.dto.response.GetMyActiveInfoResponse;
 import blacktokkies.toquiz.domain.panel.dto.response.GetMyPanelsResponse;
 import blacktokkies.toquiz.domain.panel.dto.response.PanelResponse;
 import blacktokkies.toquiz.global.common.error.RestApiException;
+import blacktokkies.toquiz.global.util.auth.CookieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,8 +21,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
+import static blacktokkies.toquiz.domain.activeinfo.exception.ActiveInfoException.NOT_EXIST_ACTIVE_INFO;
 import static blacktokkies.toquiz.domain.panel.exception.PanelErrorCode.NOT_AUTHORIZED_DELETE;
 import static blacktokkies.toquiz.domain.panel.exception.PanelErrorCode.NOT_EXIST_PANEL;
 
@@ -25,7 +32,9 @@ import static blacktokkies.toquiz.domain.panel.exception.PanelErrorCode.NOT_EXIS
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PanelService {
+    private final ActiveInfoRepository activeInfoRepository;
     private final PanelRepository panelRepository;
+    private final CookieService cookieService;
 
     @Transactional
     public PanelResponse createPanel(CreatePanelRequest createPanelRequest) {
@@ -63,6 +72,25 @@ public class PanelService {
         return PanelResponse.toDto(updatedPanel);
     }
 
+    public GetMyActiveInfoResponse getMyActiveInfo(Long panelId, String activeInfoId) {
+        checkIsExistPanel(panelId);
+
+        ActiveInfo activeInfo = getActiveInfo(activeInfoId);
+        Map<Long, ActivePanel> activePanelMap = activeInfo.getActivePanels();
+
+        // 현재 패널이 활동 정보에 존재하지 않으면 새로 추가한다.
+        if(!activePanelMap.containsKey(panelId)){
+            activePanelMap.put(panelId, new ActivePanel());
+        }
+
+        return GetMyActiveInfoResponse.toDto(activePanelMap.get(panelId));
+    }
+
+    private ActiveInfo getActiveInfo(String activeInfoId) {
+        return activeInfoRepository.findById(activeInfoId)
+            .orElseThrow(() -> new RestApiException(NOT_EXIST_ACTIVE_INFO));
+    }
+
     private void checkIsAuthorizedToDelete(Long panelId) {
         Panel panel = getPanel(panelId);
         Member member = getMember();
@@ -70,6 +98,11 @@ public class PanelService {
         if(!Objects.equals(member.getId(), panel.getMember().getId())){
             throw new RestApiException(NOT_AUTHORIZED_DELETE);
         }
+    }
+
+    private void checkIsExistPanel(Long panelId){
+        panelRepository.findById(panelId)
+            .orElseThrow(() -> new RestApiException(NOT_EXIST_PANEL));
     }
 
     public Panel getPanel(Long panelId){
