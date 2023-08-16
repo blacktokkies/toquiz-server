2160be6a30358a36dba0a3f29383aac65c4f8e4c
soomanbaek <qortnaks311@gmail.com>
2023-08-16 21:27:01 +0900

    feat: ToggleLikeQuestion Response DTO (#52)

---
+++ src/main/java/blacktokkies/toquiz/domain/question/dto/response/ToggleLikeQuestionResponse.java
@@ -0,0 +1,20 @@
+package blacktokkies.toquiz.domain.question.dto.response;
+
+import blacktokkies.toquiz.domain.question.domain.Question;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+public class ToggleLikeQuestionResponse {
+    private Long questionId;
+    private long likeNum;
+    private Boolean isActived;
+
+    public static ToggleLikeQuestionResponse toDto(Question question, boolean isActived){
+            return new ToggleLikeQuestionResponse(
+                question.getId(),
+                question.getLikeNum(),
+                isActived
+            );
+    }
+}
