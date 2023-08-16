79df7f2fb169be3627b6876afdd09de4bc50b4fe
soomanbaek <qortnaks311@gmail.com>
2023-08-16 20:29:18 +0900

    feat: ActiveInfo 엔티티 수정 (#52)
    - 엔티티에 final 키워드를 사용할 수 없다.
    - List는 미리 생성되도록 수정

--- src/main/java/blacktokkies/toquiz/domain/activeinfo/domain/ActivePanel.java
+++ src/main/java/blacktokkies/toquiz/domain/activeinfo/domain/ActivePanel.java
@@ -1,8 +1,12 @@
 package blacktokkies.toquiz.domain.activeinfo.domain;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class ActivePanel {
-    private List<Long> likedQuestionIds;
-    private List<Long> createdQuestionIds;
+    private List<Long> likedQuestionIds = new ArrayList<>();
+    private List<Long> createdQuestionIds = new ArrayList<>();
 }
