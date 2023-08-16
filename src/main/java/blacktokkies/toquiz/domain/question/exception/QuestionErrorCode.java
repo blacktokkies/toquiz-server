e34ec5b730b12e4d23b32adfc60a591761a49ae9
soomanbaek <qortnaks311@gmail.com>
2023-08-16 21:20:45 +0900

    feat: 질문 좋아요 관련 에러 코드 (#52)

--- src/main/java/blacktokkies/toquiz/domain/question/exception/QuestionErrorCode.java
+++ src/main/java/blacktokkies/toquiz/domain/question/exception/QuestionErrorCode.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum QuestionErrorCode implements ErrorCode {
     NOT_EXIST_QUESTION(HttpStatus.NOT_FOUND, "해당 질문이 존재하지 않습니다."),
+    INVALID_ACTIVE_LIKE_QUESTION(HttpStatus.BAD_REQUEST, "유효하지 않은 좋아요 활성화 요청입니다."),
+    INVALID_INACTIVE_LIKE_QUESTION(HttpStatus.BAD_REQUEST, "유효하지 않은 좋아요 비활성화 요청입니다"),
     ;
 
     private final HttpStatus httpStatus;
