c60350a8f9c688f86001d575f4f8bce8387acd0c
soomanbaek <qortnaks311@gmail.com>
2023-08-29 00:09:38 +0900

    feat: TimeZone 시간대를, `Asia/Seoul`로 설정한다

--- src/main/java/blacktokkies/toquiz/global/config/ApplicationConfig.java
+++ src/main/java/blacktokkies/toquiz/global/config/ApplicationConfig.java
@@ -1,10 +1,16 @@
 package blacktokkies.toquiz.global.config;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
-
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
