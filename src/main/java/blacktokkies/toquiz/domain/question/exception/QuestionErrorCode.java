c9dc5970504069f1c4e17afe84852e926d45b8b4
soomanbaek <qortnaks311@gmail.com>
2023-10-19 13:38:07 +0900

    feat: 자신이 생성하지 않은 질문 요청시 예외코드 (#80)

--- src/main/java/blacktokkies/toquiz/domain/question/exception/QuestionErrorCode.java
+++ src/main/java/blacktokkies/toquiz/domain/question/exception/QuestionErrorCode.java
@@ -11,6 +11,7 @@ public enum QuestionErrorCode implements ErrorCode {
     NOT_EXIST_QUESTION(HttpStatus.NOT_FOUND, "해당 질문이 존재하지 않습니다."),
     INVALID_ACTIVE_LIKE_QUESTION(HttpStatus.BAD_REQUEST, "유효하지 않은 좋아요 활성화 요청입니다."),
     INVALID_INACTIVE_LIKE_QUESTION(HttpStatus.BAD_REQUEST, "유효하지 않은 좋아요 비활성화 요청입니다"),
+    NOT_MY_CREATE_QUESTION(HttpStatus.BAD_REQUEST, "자신이 생성한 질문에 대한 요청이 아닙니다"),
     NOT_ACTIVE_QUESTION(HttpStatus.BAD_REQUEST, "사용자가 활동하지 않은 질문에 대한 잘못된 요청입니다");
     ;
 
