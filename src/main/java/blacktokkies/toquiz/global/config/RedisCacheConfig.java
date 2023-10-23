ce8090a45fb62c42f44856839bbcf267cd5472b3
soomanbaek <qortnaks311@gmail.com>
2023-10-23 20:30:04 +0900

    feat: cache 설정 파일 추가 (#91)
    - 3분의 만료기한을 설정

---
+++ src/main/java/blacktokkies/toquiz/global/config/RedisCacheConfig.java
@@ -0,0 +1,33 @@
+package blacktokkies.toquiz.global.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    @Bean
+    public CacheManager rcm(RedisConnectionFactory cf){
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+            .entryTtl(Duration.ofMinutes(3)); // 3분의 캐시 만료기한 설정
+
+        return RedisCacheManager
+            .RedisCacheManagerBuilder
+            .fromConnectionFactory(cf)
+            .cacheDefaults(redisCacheConfiguration)
+            .build();
+    }
+}
